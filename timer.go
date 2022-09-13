8bedf106b1d386034bab3954e022fe5c366cbdfd
Jean Viaene <jean.viaene@epfl.ch>
2022-09-13 12:46:36 +0200

    Add sources to main

---
+++ timer.go
@@ -0,0 +1,23 @@
+package debugsync
+
+import (
+	"time"
+)
+
+const mutexTimeout = 30 * time.Minute
+
+func startLockTimer(msg string, stack []byte) chan struct{} {
+	done := make(chan struct{})
+
+	go func(s []byte) {
+		select {
+		case <-time.After(mutexTimeout):
+			Logger.Error().Msgf("%v : %v", msg, string(s))
+			return
+		case <-done:
+			return
+		}
+	}(stack)
+
+	return done
+}
