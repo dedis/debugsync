1c9dc5802bb999d8aedef1bbe2aa3460ee090268
Jean Viaene <jean.viaene@epfl.ch>
2022-10-07 08:41:34 +0200

    WIP: improve API

--- channel/channel.go
+++ channel/channel.go
@@ -2,56 +2,100 @@ package channel
 
 import (
 	"context"
-	"fmt"
-	"runtime"
+	"errors"
+	"runtime/debug"
 	"time"
 )
 
 type TimedChannel[T any] struct {
-	c chan T
-	t time.Duration
+	c      chan T
+	t      time.Duration
+	ctx    context.Context
+	pushCb func(string)
+	popCb  func(string)
 }
 
-// NewTimedChannel creates a new cryChan of the given type and size
-func NewTimedChannel[T any](bufSize int, timeout time.Duration) TimedChannel[T] {
+// NewWithTimeout creates a new channel of the given size, type and timeout
+// Note: if the callbacks are not nil, they are called when the timeout expires
+func NewWithTimeout[T any](
+	bufSize int,
+	timeout time.Duration,
+	pushCallback func(string),
+	popCallback func(string)) TimedChannel[T] {
 	Logger = Logger.With().Int("size", bufSize).Logger()
 
 	return TimedChannel[T]{
-		c: make(chan T, bufSize),
-		t: timeout,
+		c:      make(chan T, bufSize),
+		t:      timeout,
+		ctx:    context.Background(),
+		pushCb: pushCallback,
+		popCb:  popCallback,
 	}
 }
 
-// Push adds an element in the channel, or logs if it's blocked for too long
+// NewWithContext creates a new channel of the given size, type and context
+// Note: if the callbacks are not nil, they are called when the context expires
+func NewWithContext[T any](
+	bufSize int,
+	ctx context.Context,
+	pushCallback func(string),
+	popCallback func(string)) TimedChannel[T] {
+	Logger = Logger.With().Int("size", bufSize).Logger()
+
+	return TimedChannel[T]{
+		c:      make(chan T, bufSize),
+		t:      0,
+		ctx:    ctx,
+		pushCb: pushCallback,
+		popCb:  popCallback,
+	}
+}
+
+// Push adds an element in the channel,
+// or calls the pushCb if it fails after the given timeout
 func (c *TimedChannel[T]) Push(e T) {
-	start := time.Now()
 	select {
 	case c.c <- e:
+		return
+	case <-c.ctx.Done():
 	case <-time.After(c.t):
-		// prints the first 16 bytes of the trace, which should contain at least
-		// the goroutine id.
-		trace := make([]byte, 16)
-		runtime.Stack(trace, false)
-		Logger.Warn().Str("obj", fmt.Sprintf("%+v", e)).
-			Str("trace", string(trace)).Msg("channel blocking")
-		c.c <- e
-		Logger.Warn().Str("obj", fmt.Sprintf("%+v", e)).
-			Str("elapsed", time.Since(start).String()).
-			Str("trace", string(trace)).Msg("channel unblocked")
+	}
+
+	// process timeout
+	s := string(debug.Stack())
+	if c.pushCb != nil {
+		c.pushCb(s)
+	} else {
+		Logger.Warn().Str("stack", s)
 	}
 }
 
 // Pop removes an element from the channel
-func (c *TimedChannel[T]) Pop(ctx context.Context) (t T, err error) {
+func (c *TimedChannel[T]) Pop() (t T, err error) {
 	select {
 	case el := <-c.c:
 		return el, nil
-	case <-ctx.Done():
-		return t, ctx.Err()
+	case <-c.ctx.Done():
+	case <-time.After(c.t):
 	}
+
+	// process timeout
+	s := string(debug.Stack())
+	if c.popCb != nil {
+		c.popCb(s)
+	} else {
+		Logger.Warn().Str("stack", s)
+	}
+
+	return t, errors.New(s)
 }
 
 // Len gives the current number of elements in the channel
 func (c *TimedChannel[T]) Len() int {
 	return len(c.c)
+}
+
+// Channel returns the raw channel used
+func (c *TimedChannel[T]) Channel() *chan T {
+	return &c.c
 }
--- channel/channel_test.go
+++ channel/channel_test.go
@@ -1,15 +1,85 @@
 package channel
 
 import (
-	"bytes"
 	"context"
-	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
-	"strings"
 	"testing"
 	"time"
 )
 
+func TestNewWithContext(t *testing.T) {
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*1)
+	c := NewWithContext[bool](1, ctx, nil, nil)
+	require.NotNil(t, c)
+}
+
+func TestNewWithTimeout(t *testing.T) {
+	c := NewWithTimeout[bool](1, time.Second*2, nil, nil)
+	require.NotNil(t, c)
+}
+
+/*
+func TestChannel(t *testing.T) {
+	c := NewWithTimeout[bool](1, time.Second*2, nil, nil)
+	channel := c.Channel()
+	require.IsType(t, *chan bool, (channel))
+}
+*/
+
+func TestLen(t *testing.T) {
+	c := NewWithTimeout[bool](123, time.Millisecond*1, nil, nil)
+	require.Equal(t, 0, c.Len())
+
+	c.Push(true)
+	c.Push(false)
+	require.Equal(t, 2, c.Len())
+
+	_, err := c.Pop()
+	require.NoError(t, err)
+	require.Equal(t, 1, c.Len())
+
+	_, err = c.Pop()
+	require.NoError(t, err)
+	require.Equal(t, 0, c.Len())
+
+	_, err = c.Pop()
+	require.Error(t, err)
+	require.Equal(t, 0, c.Len())
+}
+
+func TestPushWithTimeoutSuccess(t *testing.T) {
+
+}
+
+func TestPushWithContextSuccess(t *testing.T) {
+
+}
+
+func TestTimedChannelPopWithTimeoutSuccess(t *testing.T) {
+
+}
+
+func TestTimedChannelPopWithContextSuccess(t *testing.T) {
+
+}
+
+func TestTimedChannelPushWithTimeoutFail(t *testing.T) {
+
+}
+
+func TestTimedChannelPushWithContextFail(t *testing.T) {
+
+}
+
+func TestTimedChannelPopWithTimeoutFail(t *testing.T) {
+
+}
+
+func TestTimedChannelPopWithContextFail(t *testing.T) {
+
+}
+
+/*
 func TestTimedChannelNormalUse(t *testing.T) {
 	c := NewTimedChannel[bool](10, time.Second*1)
 	require.NotNil(t, c)
@@ -68,3 +138,6 @@ func TestTimedChannelTimeOut(t *testing.T) {
 	time.Sleep(time.Millisecond * 10) // the log needs some time be generated
 	require.True(t, strings.Contains(logBuffer.String(), "channel unblocked"))
 }
+
+
+*/
