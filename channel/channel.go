c47030e43655ab0b7ee3d857d5d63adf95070220
Jean Viaene <jean.viaene@epfl.ch>
2022-12-06 12:05:57 +0100

    changes after review

--- channel/channel.go
+++ channel/channel.go
@@ -2,6 +2,7 @@ package channel
 
 import (
 	"context"
+	"github.com/rs/zerolog"
 	"runtime/debug"
 	"time"
 )
@@ -9,14 +10,15 @@ import (
 const defaultChannelTimeout = time.Second * 1
 
 type Timed[T any] struct {
-	c chan T
+	c   chan T
+	log zerolog.Logger
 }
 
 type Error string
 
 const (
 	ErrFailedToSend    = Error("Could not send data on channel.")
-	ErrFailedToReceive = Error("Could not receive data from channel")
+	ErrFailedToReceive = Error("Could not receive data from channel.")
 )
 
 func (e Error) Error() string {
@@ -25,81 +27,86 @@ func (e Error) Error() string {
 
 // WithExpiration creates a new channel of the given size and type
 func WithExpiration[T any](bufSize int) Timed[T] {
-	Logger = Logger.With().Int("size", bufSize).Logger()
-
 	return Timed[T]{
-		c: make(chan T, bufSize),
+		c:   make(chan T, bufSize),
+		log: Logger.With().Int("size", bufSize).Logger(),
 	}
 }
 
-// BlockingSendWithContext adds an element in the channel,
-// or logs a warning if it fails after the given context
-func (c *Timed[T]) BlockingSendWithContext(ctx context.Context, e T) {
+// SendWithContext adds an element in the channel,
+// or logs a warning if it fails in the given context.
+// Note: this is a blocking call as it waits on a channel.
+func (c *Timed[T]) SendWithContext(ctx context.Context, e T) {
 	select {
 	case c.c <- e:
 		return
 	case <-ctx.Done():
-		Logger.Warn().Msgf("%s %X\n%s", ErrFailedToSend, c.c, string(debug.Stack()))
+		c.log.Warn().Msgf("%s %X\n%s", ErrFailedToSend, c.c, string(debug.Stack()))
 		c.c <- e
-		Logger.Info().Msgf("unblocked channel %X on send", c.c)
+		c.log.Info().Msgf("unblocked channel %X on send", c.c)
 	}
 }
 
-// BlockingSendWithTimeout adds an element in the channel,
-// or logs a warning if it fails after the given timeout
-func (c *Timed[T]) BlockingSendWithTimeout(t time.Duration, e T) {
+// SendWithTimeout adds an element in the channel,
+// or logs a warning if it fails after the given timeout.
+// Note: this is a blocking call as it waits on a channel.
+func (c *Timed[T]) SendWithTimeout(t time.Duration, e T) {
 	ctx, cancel := context.WithTimeout(context.Background(), t)
 	defer cancel()
 
-	c.BlockingSendWithContext(ctx, e)
+	c.SendWithContext(ctx, e)
 }
 
-// BlockingSend adds an element in the channel,
-// or logs a warning if it fails after default timeout
-func (c *Timed[T]) BlockingSend(e T) {
+// Send adds an element in the channel,
+// or logs a warning if it fails after default timeout.
+// Note: this is a blocking call as it waits on a channel.
+func (c *Timed[T]) Send(e T) {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultChannelTimeout)
 	defer cancel()
 
-	c.BlockingSendWithContext(ctx, e)
+	c.SendWithContext(ctx, e)
 }
 
-// BlockingReceiveWithContext removes an element from the channel
-// or logs a warning if it fails after the given context
-func (c *Timed[T]) BlockingReceiveWithContext(ctx context.Context) T {
+// ReceiveWithContext removes an element from the channel
+// or logs a warning if it fails in the given context.
+// Note: this is a blocking call as it waits on a channel.
+func (c *Timed[T]) ReceiveWithContext(ctx context.Context) T {
 	var e T
 
 	select {
 	case e = <-c.c:
 	case <-ctx.Done():
-		Logger.Warn().Msgf("%s %X\n%s", ErrFailedToReceive, c.c, string(debug.Stack()))
+		c.log.Warn().Msgf("%s %X\n%s", ErrFailedToReceive, c.c, string(debug.Stack()))
 		c.c <- e
-		Logger.Info().Msgf("unblocked channel %X on receiving", c.c)
+		c.log.Info().Msgf("unblocked channel %X on receiving", c.c)
 	}
 
 	return e
 }
 
-// BlockingReceiveWithTimeout removes an element from the channel
-// or logs a warning if it fails after the given timeout
-func (c *Timed[T]) BlockingReceiveWithTimeout(t time.Duration) T {
+// ReceiveWithTimeout removes an element from the channel
+// or logs a warning if it fails after the given timeout.
+// Note: this is a blocking call as it waits on a channel.
+func (c *Timed[T]) ReceiveWithTimeout(t time.Duration) T {
 	ctx, cancel := context.WithTimeout(context.Background(), t)
 	defer cancel()
 
-	return c.BlockingReceiveWithContext(ctx)
+	return c.ReceiveWithContext(ctx)
 }
 
-// BlockingReceive removes an element from the channel
-// or logs a warning if it fails after the default timeout
-func (c *Timed[T]) BlockingReceive() T {
+// Receive removes an element from the channel
+// or logs a warning if it fails after the default timeout.
+// Note: this is a blocking call as it waits on a channel.
+func (c *Timed[T]) Receive() T {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultChannelTimeout)
 	defer cancel()
 
-	return c.BlockingReceiveWithContext(ctx)
+	return c.ReceiveWithContext(ctx)
 }
 
-// NotBlockingSendWithContext adds an element in the channel,
-// or returns an error if it fails in the given context
-func (c *Timed[T]) NotBlockingSendWithContext(ctx context.Context, e T) error {
+// NonBlockingSendWithContext adds an element in the channel,
+// or returns an error if it fails in the given context.
+func (c *Timed[T]) NonBlockingSendWithContext(ctx context.Context, e T) error {
 	select {
 	case c.c <- e:
 		return nil
@@ -108,27 +115,27 @@ func (c *Timed[T]) NotBlockingSendWithContext(ctx context.Context, e T) error {
 	}
 }
 
-// NotBlockingSendWithTimeout adds an element in the channel,
-// or returns an error if it fails after the given timeout
-func (c *Timed[T]) NotBlockingSendWithTimeout(t time.Duration, e T) error {
+// NonBlockingSendWithTimeout adds an element in the channel,
+// or returns an error if it fails after the given timeout.
+func (c *Timed[T]) NonBlockingSendWithTimeout(t time.Duration, e T) error {
 	ctx, cancel := context.WithTimeout(context.Background(), t)
 	defer cancel()
 
-	return c.NotBlockingSendWithContext(ctx, e)
+	return c.NonBlockingSendWithContext(ctx, e)
 }
 
-// NotBlockingSend adds an element in the channel,
-// or returns an error if it fails after the default timeout
-func (c *Timed[T]) NotBlockingSend(e T) error {
+// NonBlockingSend adds an element in the channel,
+// or returns an error if it fails after the default timeout.
+func (c *Timed[T]) NonBlockingSend(e T) error {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultChannelTimeout)
 	defer cancel()
 
-	return c.NotBlockingSendWithContext(ctx, e)
+	return c.NonBlockingSendWithContext(ctx, e)
 }
 
-// NotBlockingReceiveWithContext removes an element from the channel
-// or returns an error if it fails in the given context
-func (c *Timed[T]) NotBlockingReceiveWithContext(ctx context.Context) (T, error) {
+// NonBlockingReceiveWithContext removes an element from the channel
+// or returns an error if it fails in the given context.
+func (c *Timed[T]) NonBlockingReceiveWithContext(ctx context.Context) (T, error) {
 	var e T
 
 	select {
@@ -139,22 +146,22 @@ func (c *Timed[T]) NotBlockingReceiveWithContext(ctx context.Context) (T, error)
 	}
 }
 
-// NotBlockingReceiveWithTimeout removes an element from the channel
+// NonBlockingReceiveWithTimeout removes an element from the channel
 // or returns an error if it fails after the given timeout
-func (c *Timed[T]) NotBlockingReceiveWithTimeout(t time.Duration) (e T, err error) {
+func (c *Timed[T]) NonBlockingReceiveWithTimeout(t time.Duration) (e T, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), t)
 	defer cancel()
 
-	return c.NotBlockingReceiveWithContext(ctx)
+	return c.NonBlockingReceiveWithContext(ctx)
 }
 
-// NotBlockingReceive removes an element from the channel
+// NonBlockingReceive removes an element from the channel
 // or returns an error if it fails after the default timeout
-func (c *Timed[T]) NotBlockingReceive() (e T, err error) {
+func (c *Timed[T]) NonBlockingReceive() (e T, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultChannelTimeout)
 	defer cancel()
 
-	return c.NotBlockingReceiveWithContext(ctx)
+	return c.NonBlockingReceiveWithContext(ctx)
 }
 
 // Len gives the current number of elements in the channel
--- channel/channel_test.go
+++ channel/channel_test.go
@@ -30,7 +30,7 @@ func TestWithExpiration(t *testing.T) {
 	require.NotNil(t, c)
 }
 
-func TestBlockingSendWithContextSuccess(t *testing.T) {
+func TestSendWithContextSuccess(t *testing.T) {
 	l := setupLogger()
 	defer restoreLogger()
 
@@ -39,31 +39,31 @@ func TestBlockingSendWithContextSuccess(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
 	defer cancel()
 
-	c.BlockingSendWithContext(ctx, 0)
+	c.SendWithContext(ctx, 0)
 	require.False(t, strings.Contains(l.String(), ErrFailedToSend.Error()))
 }
 
-func TestBlockingSendWithTimeoutSuccess(t *testing.T) {
+func TestSendWithTimeoutSuccess(t *testing.T) {
 	l := setupLogger()
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
-	c.BlockingSendWithTimeout(time.Millisecond, 0)
+	c.SendWithTimeout(time.Millisecond, 0)
 	require.False(t, strings.Contains(l.String(), ErrFailedToSend.Error()))
 }
 
-func TestBlockingSendSuccess(t *testing.T) {
+func TestSendSuccess(t *testing.T) {
 	l := setupLogger()
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
-	c.BlockingSend(0)
+	c.Send(0)
 	require.False(t, strings.Contains(l.String(), ErrFailedToSend.Error()))
 }
 
-func TestBlockingReceiveWithContextSuccess(t *testing.T) {
+func TestReceiveWithContextSuccess(t *testing.T) {
 	l := setupLogger()
 	defer restoreLogger()
 
@@ -73,48 +73,48 @@ func TestBlockingReceiveWithContextSuccess(t *testing.T) {
 	defer cancel()
 
 	expectedValue := -1
-	c.BlockingSend(expectedValue)
-	v := c.BlockingReceiveWithContext(ctx)
+	c.Send(expectedValue)
+	v := c.ReceiveWithContext(ctx)
 	require.False(t, strings.Contains(l.String(), ErrFailedToReceive.Error()))
 	require.Equal(t, expectedValue, v)
 }
 
-func TestBlockingReceiveWithTimeoutSuccess(t *testing.T) {
+func TestReceiveWithTimeoutSuccess(t *testing.T) {
 	l := setupLogger()
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
 	expectedValue := 2
-	c.BlockingSend(expectedValue)
-	v := c.BlockingReceiveWithTimeout(time.Millisecond)
+	c.Send(expectedValue)
+	v := c.ReceiveWithTimeout(time.Millisecond)
 	require.False(t, strings.Contains(l.String(), ErrFailedToReceive.Error()))
 	require.Equal(t, expectedValue, v)
 }
 
-func TestBlockingReceiveSuccess(t *testing.T) {
+func TestReceiveSuccess(t *testing.T) {
 	l := setupLogger()
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
 	expectedValue := -7
-	c.BlockingSend(expectedValue)
-	v := c.BlockingReceive()
+	c.Send(expectedValue)
+	v := c.Receive()
 	require.False(t, strings.Contains(l.String(), ErrFailedToReceive.Error()))
 	require.Equal(t, expectedValue, v)
 }
 
-func TestBlockingSendFail(t *testing.T) {
+func TestSendFail(t *testing.T) {
 	l := setupLogger()
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
-	c.BlockingSendWithTimeout(time.Millisecond, 0)
+	c.SendWithTimeout(time.Millisecond, 0)
 
 	go func() {
-		c.BlockingSendWithTimeout(time.Millisecond, 0)
+		c.SendWithTimeout(time.Millisecond, 0)
 	}()
 
 	// need a looong time on Windows to see the logs in the buffer
@@ -122,14 +122,14 @@ func TestBlockingSendFail(t *testing.T) {
 	require.True(t, strings.Contains(l.String(), ErrFailedToSend.Error()))
 }
 
-func TestBlockingReceiveFail(t *testing.T) {
+func TestReceiveFail(t *testing.T) {
 	l := setupLogger()
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
 	go func() {
-		c.BlockingReceiveWithTimeout(time.Millisecond)
+		c.ReceiveWithTimeout(time.Millisecond)
 	}()
 
 	// need a looong time on Windows to see the logs in the buffer
@@ -137,7 +137,7 @@ func TestBlockingReceiveFail(t *testing.T) {
 	require.True(t, strings.Contains(l.String(), ErrFailedToReceive.Error()))
 }
 
-func TestNotBlockingSendWithContextSuccess(t *testing.T) {
+func TestNonBlockingSendWithContextSuccess(t *testing.T) {
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
@@ -145,29 +145,29 @@ func TestNotBlockingSendWithContextSuccess(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
 	defer cancel()
 
-	err := c.NotBlockingSendWithContext(ctx, 0)
+	err := c.NonBlockingSendWithContext(ctx, 0)
 	require.NoError(t, err)
 }
 
-func TestNotBlockingSendWithTimeoutSuccess(t *testing.T) {
+func TestNonBlockingSendWithTimeoutSuccess(t *testing.T) {
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
-	err := c.NotBlockingSendWithTimeout(time.Millisecond, 0)
+	err := c.NonBlockingSendWithTimeout(time.Millisecond, 0)
 	require.NoError(t, err)
 }
 
-func TestNotBlockingSendSuccess(t *testing.T) {
+func TestNonBlockingSendSuccess(t *testing.T) {
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
-	err := c.NotBlockingSend(0)
+	err := c.NonBlockingSend(0)
 	require.NoError(t, err)
 }
 
-func TestNotBlockingReceiveWithContextSuccess(t *testing.T) {
+func TestNonBlockingReceiveWithContextSuccess(t *testing.T) {
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
@@ -176,52 +176,52 @@ func TestNotBlockingReceiveWithContextSuccess(t *testing.T) {
 	defer cancel()
 
 	expectedValue := -1
-	err := c.NotBlockingSend(expectedValue)
+	err := c.NonBlockingSend(expectedValue)
 	require.NoError(t, err)
 
-	v, err := c.NotBlockingReceiveWithContext(ctx)
+	v, err := c.NonBlockingReceiveWithContext(ctx)
 	require.NoError(t, err)
 	require.Equal(t, expectedValue, v)
 }
 
-func TestNotBlockingReceiveWithTimeoutSuccess(t *testing.T) {
+func TestNonBlockingReceiveWithTimeoutSuccess(t *testing.T) {
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
 	expectedValue := 2
-	err := c.NotBlockingSend(expectedValue)
+	err := c.NonBlockingSend(expectedValue)
 	require.NoError(t, err)
 
-	v, err := c.NotBlockingReceiveWithTimeout(time.Millisecond)
+	v, err := c.NonBlockingReceiveWithTimeout(time.Millisecond)
 	require.NoError(t, err)
 	require.Equal(t, expectedValue, v)
 }
 
-func TestNotBlockingReceiveSuccess(t *testing.T) {
+func TestNonBlockingReceiveSuccess(t *testing.T) {
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
 	expectedValue := -7
-	err := c.NotBlockingSend(expectedValue)
+	err := c.NonBlockingSend(expectedValue)
 	require.NoError(t, err)
 
-	v, err := c.NotBlockingReceive()
+	v, err := c.NonBlockingReceive()
 	require.NoError(t, err)
 	require.Equal(t, expectedValue, v)
 }
 
-func TestNotBlockingSendFail(t *testing.T) {
+func TestNonBlockingSendFail(t *testing.T) {
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
-	err := c.NotBlockingSendWithTimeout(time.Millisecond, 0)
+	err := c.NonBlockingSendWithTimeout(time.Millisecond, 0)
 	require.NoError(t, err)
 
 	go func() {
-		err := c.NotBlockingSendWithTimeout(time.Millisecond, 0)
+		err := c.NonBlockingSendWithTimeout(time.Millisecond, 0)
 		require.Equal(t, err, ErrFailedToSend)
 	}()
 
@@ -229,13 +229,13 @@ func TestNotBlockingSendFail(t *testing.T) {
 	time.Sleep(time.Millisecond * 100)
 }
 
-func TestNotBlockingReceiveFail(t *testing.T) {
+func TestNonBlockingReceiveFail(t *testing.T) {
 	defer restoreLogger()
 
 	c := WithExpiration[int](1)
 
 	go func() {
-		_, err := c.NotBlockingReceiveWithTimeout(time.Millisecond)
+		_, err := c.NonBlockingReceiveWithTimeout(time.Millisecond)
 		require.Error(t, err, ErrFailedToReceive)
 	}()
 
@@ -248,7 +248,7 @@ func TestChannel(t *testing.T) {
 	channel := c.Channel()
 
 	const data = 12345
-	c.BlockingSend(data)
+	c.Send(data)
 
 	value := <-channel
 	require.Equal(t, data, value)
@@ -258,13 +258,13 @@ func TestLen(t *testing.T) {
 	c := WithExpiration[bool](3)
 	require.Equal(t, 0, c.Len())
 
-	c.BlockingSend(true)
-	c.BlockingSend(false)
+	c.Send(true)
+	c.Send(false)
 	require.Equal(t, 2, c.Len())
 
-	c.BlockingReceive()
+	c.Receive()
 	require.Equal(t, 1, c.Len())
 
-	c.BlockingReceive()
+	c.Receive()
 	require.Equal(t, 0, c.Len())
 }
